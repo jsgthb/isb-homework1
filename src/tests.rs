fa0b6e3c0af30e2c351be33707bd66d68e453f48
jsgthb <jeff.schaack@gmail.com>
2024-10-25 23:15:09 +0200

    Moved round_trans test case to test.rs

--- src/tests.rs
+++ src/tests.rs
@@ -112,4 +112,35 @@ mod tests {
         aes.print_state();
         assert_eq!(aes.state.get(3, 3), 0xFF);
     }
+
+    #[test]
+    fn test_aes_round_trans() {
+        // Plaintext and roundkey
+        let plaintext_array: [u8; 16] = [
+            0x01, 0x23, 0x45, 0x67, 
+            0x89, 0xab, 0xcd, 0xef, 
+            0xfe, 0xdc, 0xba, 0x98, 
+            0x76, 0x54, 0x32, 0x10
+        ];
+        let round_key_array: [u8; 16] = [
+            0x0f, 0x15, 0x71, 0xc9, 
+            0x47, 0xd9, 0xe8, 0x59, 
+            0x0c, 0xb7, 0xad, 0xd6, 
+            0xaf, 0x7f, 0x67, 0x98
+        ];
+        // Start AES operations
+        let plaintext_matrix = Matrix::from_array(plaintext_array);
+        let roundkey_matrix = Matrix::from_array(round_key_array);
+        let mut aes = AES::from_matrix(plaintext_matrix);
+        aes.round_trans(&roundkey_matrix);
+        let ciphertext_generated = aes.state.to_array();
+        let ciphertext_expected = [
+            0xb9, 0xe4, 0x47, 0xc5, 
+            0x94, 0x8e, 0x20, 0xd6, 
+            0x57, 0x16, 0x9a, 0xf5, 
+            0x75, 0x51, 0x3f, 0x3b
+        ];
+        println!("Expected ciphertext: {:x?}\n", ciphertext_expected);
+        assert_eq!(ciphertext_generated, ciphertext_expected)
+    }
 }
